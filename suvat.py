ba342650fa260aa21b72c78e557e199fbe107186
puggam3er <69014858+puggam3er@users.noreply.github.com>
2022-02-11 17:11:21 +0000

    Add files via upload

---
+++ suvat.py
@@ -0,0 +1,14 @@
+def solve():
+    x = input("What do you want to find?")
+    if not x:
+        print("Empty field.")
+    else:
+        s = float(input("s:"))
+        u = float(input("u:"))
+        v = float(input("v:"))
+        a = float(input("a:"))
+        t = float(input("t:"))
+    return
+
+
+solve()
